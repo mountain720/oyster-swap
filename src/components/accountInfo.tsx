749be2c89fba764da1a48f1b54fcc4e9b167a541
Mountain720 <70184721+mountain720@users.noreply.github.com>
2021-03-11 22:17:28 -0700

    Update accountInfo.tsx

--- src/components/accountInfo.tsx
+++ src/components/accountInfo.tsx
@@ -3,7 +3,7 @@ import { useWallet } from "../context/wallet";
 import { shortenAddress } from "./../utils/utils";
 import { Identicon } from "./identicon";
 import { useNativeAccount } from "./../utils/accounts";
-import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { LAMPORTS_PER_SAFE } from "@safecoin/web3.js";
 
 export const AccountInfo = (props: {}) => {
   const { wallet } = useWallet();
@@ -16,7 +16,7 @@ export const AccountInfo = (props: {}) => {
   return (
     <div className="wallet-wrapper">
       <span>
-        {((account?.lamports || 0) / LAMPORTS_PER_SOL).toFixed(6)} SOL
+        {((account?.lamports || 0) / LAMPORTS_PER_SAFE).toFixed(6)} SAFE
       </span>
       <div className="wallet-key">
         {shortenAddress(`${wallet.publicKey}`)}
